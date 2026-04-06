590323af1392b093d3b281b7c2c2caefeb0e0319
Laura-th1 <laut15@outlook.com>
2026-04-05 22:21:10 -0500

    probando process.php

--- controllers/process.php
+++ controllers/process.php
@@ -1,9 +1,11 @@
+@ -1,43 +1,51 @@
 <?php
 require_once "../config/bd.php";
 
 header("Content-Type: text/xml");
 
 //debug temporal
+
 @file_put_contents("log.txt", print_r($_POST, true) . "\n--- " . date('Y-m-d H:i:s') . " ---\n", FILE_APPEND);
 
 $telefono = $_POST['From'] ?? '';
@@ -23,6 +25,7 @@ echo "<Response>";
 if ($step == 1) {
 
     // Guardar nombre
+   
     $query = "INSERT INTO respuestas (telefono, pregunta, respuesta) VALUES ('" . pg_escape_string($telefono) . "', 'Nombre', '" . pg_escape_string($respuesta) . "')";
     $result = pg_query($conn, $query);
     
@@ -35,14 +38,18 @@ if ($step == 1) {
     echo "<Gather input='speech' method='POST' timeout='5'
           action='https://ivr-3knv.onrender.com/controllers/process.php?step=2'></Gather>";
 
-} 
+} elseif ($step == 2) {
 
-if ($curlError) {
-    echo "<Say voice='Polly.Lupe'>Tus datos fueron guardados, pero hubo un error al enviar el mensaje.</Say>";
-} elseif ($httpCode == 201) {
-    echo "<Say voice='Polly.Lupe'>Tus datos fueron guardados y el mensaje fue enviado correctamente.</Say>";
-} else {
-    echo "<Say voice='Polly.Lupe'>Tus datos fueron guardados, pero el mensaje no pudo enviarse.</Say>";
+    // Guardar edad
+   
+    $query = "INSERT INTO respuestas (telefono, pregunta, respuesta) VALUES ('" . pg_escape_string($telefono) . "', 'Edad', '" . pg_escape_string($respuesta) . "')";
+    $result = pg_query($conn, $query);
+    
+    if (!$result) {
+        echo "<Say voice='Polly.Lupe'>Error al guardar edad.</Say>";
+    } else {
+        echo "<Say voice='Polly.Lupe'>Gracias. Tus datos han sido guardados correctamente.</Say>";
+    }
 }
 
 echo "</Response>";
